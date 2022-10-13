62bd5d2aab86e2403ac20ef651b4f4ca24a3d861
peterclermy232 <peterclermy232@gmail.com>
2022-10-13 13:19:27 +0300

    import stylesheets

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import './stylesheets/SignupLogin.css';
 import './stylesheets/Submit.css';
 
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,7 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
