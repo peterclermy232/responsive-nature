506b4a3f91e01d1c454e8e2c72d55a9da24c0973
peterclermy232 <peterclermy232@gmail.com>
2022-10-13 13:09:11 +0300

    create get method

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  post "/"
+  
 
   get '*path',
       to: 'fallback#index',
